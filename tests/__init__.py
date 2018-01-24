2c927cf8a82e8e8412c74c87cf3b9e2ca0ed1659
Sean Dague <sean@dague.net>
2018-01-24 05:57:56 -0500

    Set logging to DEBUG by default on tests
    When tests fail in travis-ci the logging is pretty sparse, it defaults
    to INFO by default, and there are no timestamps in the logs. This does
    some very basic logging config to add that information into tests. The
    alternative would be to load pytest fixtures, but that would require
    touching lots of tests directly, where this provides a global
    solution.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,10 @@
 """Tests for Home Assistant."""
+
+# By default any tests running the fake home assistant only run at
+# INFO mode, and don't include timestamps. This makes it hard to see
+# what is going on in travis-ci failures. Make the log format more
+# like real home assistant, and make the default the DEBUG level.
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
